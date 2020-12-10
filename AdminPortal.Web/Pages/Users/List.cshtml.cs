0033add2e0e19e3d6122b1954a5b32bf4bff6408
Uchchay Dugal <kesarling@outlook.com>
2020-12-10 20:17:48 +0530

    Search functionality complete

--- AdminPortal.Web/Pages/Users/List.cshtml.cs
+++ AdminPortal.Web/Pages/Users/List.cshtml.cs
@@ -5,24 +5,41 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
 using System.Collections.Generic;
 using System.Threading.Tasks;
+using System.Linq;
+using System.ComponentModel.DataAnnotations;
 
 namespace AdminPortal.Web.Pages.Users
 {
-	[Authorize]
+	// [Authorize]
 	public class IndexModel : PageModel
     {
         private readonly AdminPortalDbContext _context;
 
-        public IndexModel(AdminPortalDbContext context)
+        [DataType(DataType.PhoneNumber)]
+		public string PhoneNumber { get; set; }
+
+
+		public IndexModel(AdminPortalDbContext context)
         {
             _context = context;
         }
 
         public new IList<User> User { get;set; }
 
-        public async Task OnGetAsync()
+        public async Task OnGetAsync(string phoneNumber)
         {
-            User = await _context.Users.ToListAsync();
+            PhoneNumber = phoneNumber;
+            if (string.IsNullOrEmpty(PhoneNumber))
+            {
+                User = await _context.Users.ToListAsync();
+            }
+            else
+			{
+                var _user = from user in await _context.Users.ToListAsync()
+                            where user.PhoneNumber.Contains(PhoneNumber)
+                            select user;
+                User = _user.ToList();
+			}
         }
     }
 }
