0c9abc8cb8b3e2e0cda112a25c0c4f1c88994246
kesarling <kesarling@outlook.com>
2021-01-20 10:14:02 +0530

    fixed the search (changed page -> view)

--- AdminPortal.Web/Pages/Home.cshtml.cs
+++ AdminPortal.Web/Pages/Home.cshtml.cs
@@ -1,9 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace AdminPortal.Web.Pages
 {
 	public class HomeModel : PageModel
 	{
+		[BindProperty(SupportsGet = true)]
+		public string SearchString { get; set; }
 
 		public void OnGet()
 		{
