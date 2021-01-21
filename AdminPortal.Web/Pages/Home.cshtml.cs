586a7335ca4814cec4a8aa154596fe1877a77440
kesarling <kesarling@outlook.com>
2021-01-21 08:37:04 +0530

    Search finally "get"s correctly

--- AdminPortal.Web/Pages/Home.cshtml.cs
+++ AdminPortal.Web/Pages/Home.cshtml.cs
@@ -5,9 +5,6 @@ namespace AdminPortal.Web.Pages
 {
 	public class HomeModel : PageModel
 	{
-		[BindProperty(SupportsGet = true)]
-		public string SearchString { get; set; }
-
 		public void OnGet()
 		{
 
