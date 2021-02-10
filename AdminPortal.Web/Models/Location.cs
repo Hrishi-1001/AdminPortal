36a12bc5b7119918a99ba288e5ce1bf04170b703
kesarling <kesarling@outlook.com>
2021-02-10 09:23:39 +0530

    Added Search by ID functionality; amend: deleted installations are now not shown on map

---
+++ AdminPortal.Web/Models/Location.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AdminPortal.Web.Models
+{
+	public class Location
+	{
+		[Display(Name = "Location")]
+		public string Name { get; set; }
+		[Display(Name = "Latitude")]
+		public double Latitude { get; set; }
+		[Display(Name = "Longitude")]
+		public double Longitude { get; set; }
+	}
+}
