319976923a225d3f4479566712117b27a76c5e6d
kesarling <kesarling@outlook.com>
2021-02-16 19:05:32 +0530

    Location adding now functional!

---
+++ AdminPortal.Web/Controllers/LocationsController.cs
@@ -0,0 +1,42 @@
+﻿using AdminPortal.Web.Data;
+using AdminPortal.Web.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AdminPortal.Web.Controllers
+{
+	public class LocationsController : Controller
+	{
+		private readonly DatabaseContext databaseContext;
+
+		public LocationsController(Data.DatabaseContext databaseContext)
+		{
+			this.databaseContext = databaseContext;
+		}
+
+		[Route("/Locations/Add")]
+		public IActionResult Add()
+		{
+			return View();
+		}
+
+		// POST: LocationsController/Create
+		[HttpPost]
+		[Route("/Locations/Add")]
+		[ValidateAntiForgeryToken]
+		public async Task<IActionResult> Create([Bind("Id", "Name")] Location location)
+		{
+			if (ModelState.IsValid && !databaseContext.Locations.Where(o => o.Id.Equals(location.Id)).Any())
+			{
+				databaseContext.Locations.Add(location);
+				await databaseContext.SaveChangesAsync();
+				return RedirectToAction("Create", "Assets");
+			}
+			return RedirectToAction("Create", "Assets");
+		}
+	}
+}
